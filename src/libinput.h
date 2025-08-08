mix-mirror/libinput

commit d557a649fd3a9fdad053ef7164b0a6f4717b20c2

Add a public plugin system to libinput
This patch adds a new API for enabling public "plugins" in libinput, in
addition to the exisitng internal ones. The API is currently limited to
specifying which paths should be loaded and whether to load them.
Public plugins are static, they are loaded before the context is initialized
and do not update after that.

If plugins are to be loaded, libinput will then run through those paths,
look up files and pass them to (future) plugins to load the actual
files.

Our debugging tools have an --enable-plugins and
--disable-plugins option that load from the default data paths
(/etc/libinput/plugins and /usr/lib{64}/libinput/plugins) and from
the $builddir/plugins directory.

Part-of: <https://gitlab.freedesktop.org/libinput/libinput/-/merge_requests/1192>

src/libinput.h
@@ -3693,6 +3693,89 @@ libinput_path_remove_device(struct libinput_device *device);
 /**
  * @ingroup base
  *
+ * Appends the given directory path to the libinput plugin lookup path.
+ * If the path is already in the lookup paths, it is ignored.
+ *
+ * A path's priority is determined by its position in the list; the first
+ * path in the list has the highest priority.
+ *
+ * Plugin lookup is performed across all paths in lexical order. If
+ * a plugin exists in multiple paths, the one in the highest priority
+ * path (i.e. front of the list) is used.
+ *
+ * Paths are not traversed recursively.
+ *
+ * Plugins that have a 0 byte size shadow any plugins with the same name
+ * but do not provide any fuctionality. This allows disabling a plugin
+ * by simply dropping an empty file in a higher-priority directory.
+ *
+ * This function must be called before i
+ * libinput_plugin_system_load_plugins().
+ *
+ * @see libinput_plugin_system_append_default_paths
+ *
+ * @since 1.29
+ */
+void
+libinput_plugin_system_append_path(struct libinput *libinput, const char *path);
+
+/**
+ * @ingroup base
+ *
+ * Add the default plugin lookup paths, typically:
+ * - /etc/libinput/plugins/
+ * - /usr/lib{64}/libinput/plugins/
+ *
+ * These paths are inserted at the current priority - to add
+ * paths with a higher priority than these, call
+ * libinput_plugin_system_append_path() prior to this function.
+ *
+ * See libinput_plugin_system_append_path() for more details.
+ *
+ * This function must be called before
+ * libinput_plugin_system_load_plugins().
+ *
+ * @see libinput_plugin_system_append_paths
+ *
+ * @since 1.29
+ */
+void
+libinput_plugin_system_append_default_paths(struct libinput *libinput);
+
+enum libinput_plugins_flags {
+	LIBINPUT_PLUGIN_FLAG_NONE = 0,
+};
+
+/**
+ * @ingroup base
+ *
+ * Load the plugins from the set of lookup paths. This function does nothing
+ * if no plugin paths have been configured, see
+ * libinput_plugin_system_append_default_paths() and
+ * libinput_plugin_system_append_path().
+ *
+ * The typical use of this function is:
+ * ```
+ * struct libinput *li = libinput_udev_create_context(...);
+ * libinput_plugin_system_append_default_paths(li);
+ * libinput_plugin_system_load(li, flags);
+ * ```
+ *
+ * This function must be called before libinput iterates through the
+ * devices, i.e. before libinput_udev_assign_seat() or libinput_path_add_device().
+ *
+ * @return 0 or a negative errno on failure
+ * @retval -ENOSYS libinput was compiled without plugin support
+ *
+ * @since 1.29
+ */
+int
+libinput_plugin_system_load_plugins(struct libinput *libinput,
+				    enum libinput_plugins_flags flags);
+
+/**
+ * @ingroup base
+ *
  * libinput keeps a single file descriptor for all events. Call into
  * libinput_dispatch() if any events become available on this fd.
  *

completion/zsh/_libinput
@@ -93,6 +93,9 @@ __all_seats()
 		+ '(natural-scrolling)' \
 		'--enable-natural-scrolling[Enable natural scrolling]' \
 		'--disable-natural-scrolling[Disable natural scrolling]' \
+		+ '(plugins)' \
+		'--enable-plugins[Enable plugins]' \
+		'--disable-plugins[Disable plugins]' \
 		+ '(tap-to-click)' \
 		'--enable-tap[Enable tap-to-click]' \
 		'--disable-tap[Disable tap-to-click]'

meson.build
@@ -168,6 +168,8 @@ dep_libevdev = dependency('libevdev', version: '>= 1.10.0')
 dep_lm = cc.find_library('m', required : false)
 dep_rt = cc.find_library('rt', required : false)
 
+config_h.set10('HAVE_PLUGINS', true)
+
 # Include directories
 includes_include = include_directories('include')
 includes_src = include_directories('src')
@@ -380,6 +382,9 @@ endif
 if get_option('internal-event-debugging')
 	config_h.set('EVENT_DEBUGGING', 1)
 endif
+
+config_h.set_quoted('LIBINPUT_PLUGIN_LIBDIR', dir_lib / 'libinput' / 'plugins')
+config_h.set_quoted('LIBINPUT_PLUGIN_ETCDIR', dir_etc / 'libinput' / 'plugins')
 
 install_headers('src/libinput.h')
 src_libinput = src_libfilter + [

src/libinput-plugin.c
@@ -327,6 +327,55 @@ libinput_plugin_notify_device_removed(struct libinput_plugin *plugin,
 		plugin->interface->device_removed(plugin, device);
 }
 
+LIBINPUT_EXPORT void
+libinput_plugin_system_append_path(struct libinput *libinput, const char *path)
+{
+	if (libinput->plugin_system.loaded) {
+		log_bug_client(libinput, "plugin system already initialized\n");
+		return;
+	}
+
+	if (strv_find(libinput->plugin_system.directories, path, NULL))
+		return;
+
+	libinput->plugin_system.directories =
+		strv_append_strdup(libinput->plugin_system.directories, path);
+}
+
+LIBINPUT_EXPORT void
+libinput_plugin_system_append_default_paths(struct libinput *libinput)
+{
+	if (libinput->plugin_system.loaded) {
+		log_bug_client(libinput, "plugin system already initialized\n");
+		return;
+	}
+
+	libinput_plugin_system_append_path(libinput, LIBINPUT_PLUGIN_ETCDIR);
+	libinput_plugin_system_append_path(libinput, LIBINPUT_PLUGIN_LIBDIR);
+}
+
+LIBINPUT_EXPORT int
+libinput_plugin_system_load_plugins(struct libinput *libinput,
+				    enum libinput_plugins_flags flags)
+{
+	if (libinput->plugin_system.loaded) {
+		log_bug_client(libinput, "%s() called twice\n", __func__);
+		return 0;
+	}
+
+	libinput_plugin_system_load_internal_plugins(libinput,
+						     &libinput->plugin_system);
+	libinput->plugin_system.loaded = true;
+
+	libinput_plugin_system_run(&libinput->plugin_system);
+
+#if !HAVE_PLUGINS
+	return -ENOSYS;
+#else
+	return 0;
+#endif
+}
+
 void
 libinput_plugin_system_run(struct libinput_plugin_system *system)
 {
@@ -403,6 +452,7 @@ libinput_plugin_system_load_internal_plugins(struct libinput *libinput,
 	 * actually connected to anything yet */
 	libinput_evdev_dispatch_plugin(libinput);
 }
+
 void
 libinput_plugin_system_destroy(struct libinput_plugin_system *system)
 {

src/libinput.sym
@@ -374,3 +374,9 @@ LIBINPUT_1.29 {
 	libinput_tablet_tool_config_eraser_button_set_button;
 	libinput_tablet_tool_config_eraser_button_set_mode;
 } LIBINPUT_1.28;
+
+LIBINPUT_1.30 {
+	libinput_plugin_system_append_default_paths;
+	libinput_plugin_system_append_path;
+	libinput_plugin_system_load_plugins;
+} LIBINPUT_1.29;

test/litest.c
@@ -2144,6 +2144,8 @@ litest_create_context(void)
 	if (verbose)
 		libinput_log_set_priority(libinput, LIBINPUT_LOG_PRIORITY_DEBUG);
 
+	libinput_plugin_system_load_plugins(libinput, LIBINPUT_PLUGIN_FLAG_NONE);
+
 	return libinput;
 }
 

tools/libinput-debug-events.c
@@ -319,7 +319,13 @@ main(int argc, char **argv)
 	if (verbose)
 		printf("libinput version: %s\n", LIBINPUT_VERSION);
 
-	li = tools_open_backend(backend, seat_or_devices, verbose, &grab);
+	bool with_plugins = (options.plugins == 1);
+	li = tools_open_backend(backend,
+				seat_or_devices,
+				verbose,
+				&grab,
+				with_plugins,
+				steal(&options.plugin_paths));
 	if (!li)
 		return EXIT_FAILURE;
 

tools/libinput-debug-gui.c
@@ -2007,7 +2007,13 @@ main(int argc, char **argv)
 		backend = BACKEND_UDEV;
 	}
 
-	li = tools_open_backend(backend, seat_or_device, verbose, &w.grab);
+	bool with_plugins = (options.plugins == 1);
+	li = tools_open_backend(backend,
+				seat_or_device,
+				verbose,
+				&w.grab,
+				with_plugins,
+				steal(&options.plugin_paths));
 	if (!li)
 		return EXIT_FAILURE;
 

commit c0519c3b5e2d7777b27c861fc40dc5477741cf5c

Correct the @since tags for the new plugin functions
Fixes: d557a649fd3a ("Add a public plugin system to libinput")
Part-of: <https://gitlab.freedesktop.org/libinput/libinput/-/merge_requests/1295>

src/libinput.h
@@ -3714,7 +3714,7 @@ libinput_path_remove_device(struct libinput_device *device);
  *
  * @see libinput_plugin_system_append_default_paths
  *
- * @since 1.29
+ * @since 1.30
  */
 void
 libinput_plugin_system_append_path(struct libinput *libinput, const char *path);
@@ -3737,7 +3737,7 @@ libinput_plugin_system_append_path(struct libinput *libinput, const char *path);
  *
  * @see libinput_plugin_system_append_paths
  *
- * @since 1.29
+ * @since 1.30
  */
 void
 libinput_plugin_system_append_default_paths(struct libinput *libinput);
@@ -3767,7 +3767,7 @@ enum libinput_plugins_flags {
  * @return 0 or a negative errno on failure
  * @retval -ENOSYS libinput was compiled without plugin support
  *
- * @since 1.29
+ * @since 1.30
  */
 int
 libinput_plugin_system_load_plugins(struct libinput *libinput,