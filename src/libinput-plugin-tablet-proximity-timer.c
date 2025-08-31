mix-mirror/libinput

commit b2cd9c69a05bb8f2e95066134495269c888e4144

plugin: remove the event frame callbacks when disabling a plugin
In all cases we remove the device's handling from the plugin so let's
remove the event frame callback for this device.

Part-of: <https://gitlab.freedesktop.org/libinput/libinput/-/merge_requests/1300>

src/libinput-plugin-tablet-proximity-timer.c
@@ -186,6 +186,9 @@ proximity_timer_plugin_device_handle_frame(struct libinput_plugin *libinput_plug
 		case EVDEV_BTN_TOOL_FINGER:
 		case EVDEV_BTN_TOOL_MOUSE:
 		case EVDEV_BTN_TOOL_LENS:
+			libinput_plugin_enable_device_event_frame(libinput_plugin,
+								  device->device,
+								  false);
 			plugin_device_destroy(device);
 			return;
 		default:

src/libinput-plugin-button-debounce.c
@@ -205,6 +205,9 @@ debounce_maybe_disable(struct plugin_device *device)
 				 "%s: disabled button debouncing on request\n",
 				 libinput_device_get_name(device->device));
 		device->state = DEBOUNCE_STATE_DISABLED;
+		libinput_plugin_enable_device_event_frame(device->parent->plugin,
+							  device->device,
+							  false);
 	}
 }
 

src/libinput-plugin-mouse-wheel.c
@@ -149,12 +149,13 @@ wheel_maybe_disable(struct plugin_device *device)
 	if (device->state != WHEEL_STATE_NONE)
 		return;
 
-	if (device->want_feature_disabled &&
-	    device->ignore_small_hi_res_movements != PASSTHROUGH) {
+	if (device->want_feature_disabled) {
 		plugin_log_debug(device->parent->plugin,
 				 "%s: disabled wheel debouncing on request\n",
 				 libinput_device_get_name(device->device));
-		device->ignore_small_hi_res_movements = PASSTHROUGH;
+		libinput_plugin_enable_device_event_frame(device->parent->plugin,
+							  device->device,
+							  false);
 		libinput_plugin_timer_cancel(device->scroll_timer);
 		device->scroll_timer =
 			libinput_plugin_timer_unref(device->scroll_timer);

src/libinput-plugin-tablet-double-tool.c
@@ -217,6 +217,9 @@ double_tool_plugin_device_handle_frame(struct libinput_plugin *libinput_plugin,
 				libinput_plugin,
 				"device %s: device is fine, unregistering device\n",
 				libinput_device_get_name(device->device));
+			libinput_plugin_enable_device_event_frame(libinput_plugin,
+								  device->device,
+								  false);
 			plugin_device_destroy(device);
 			return;
 		}

commit ce1112c263b770a118423d9e1fd2f7e8ecfa0f4d

plugin: remove the proximity timer callback after prox out events
One of the code paths was addressed in b2cd9c69a05b but this path was
missing.

Fixes: b2cd9c69a05b ("plugin: remove the event frame callbacks when disabling a plugin")
Part-of: <https://gitlab.freedesktop.org/libinput/libinput/-/merge_requests/1306>

src/libinput-plugin-tablet-proximity-timer.c
@@ -208,6 +208,9 @@ proximity_timer_plugin_device_handle_frame(struct libinput_plugin *libinput_plug
 			plugin_log_debug(libinput_plugin,
 					 "%s: proximity out timer unloaded\n",
 					 libinput_device_get_name(device->device));
+			libinput_plugin_enable_device_event_frame(libinput_plugin,
+								  device->device,
+								  false);
 			plugin_device_destroy(device);
 			return;
 		}