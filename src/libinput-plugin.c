mix-mirror/libinput

commit 9e37bc0cfa4d975291e5a2899e148fb83526d4a2

plugins: add support for lua plugins to change evdev event streams
This patch adds support for Lua scripts to modify evdev devices and
event frames before libinput sees those events.

Plugins in Lua are sandboxed and restricted in what they can do: no IO,
no network, not much of anything else.

A plugin is notified about a new device before libinput handles it and
it can thus modify that device (changes are passed through to our libevdev
context). A plugin can then also connect an evdev frame handler which
gives it access to the evdev frames before libinput processes them. The
example plugin included shows how to swap left/right mouse buttons:

    libinput:register({1})

    function frame(device, frame)
        for _, v in ipairs(frame.events) do
            if v.usage == evdev.BTN_RIGHT then
                v.usage = evdev.BTN_LEFT
            elseif v.usage == evdev.BTN_LEFT then
                v.usage = evdev.BTN_RIGHT
            end
        end
        return frame
    end

    function device_new(plugin, device)
        local usages = device:usages()
        if usages[evdev.BTN_LEFT] and usages[evdev.BTN_RIGHT] then
            device:connect("evdev-frame", frame)
        end
    end

    libinput:connect("new-evdev-device", device_new)

A few other example plugins are included in this patch

Part-of: <https://gitlab.freedesktop.org/libinput/libinput/-/merge_requests/1192>

meson.build
@@ -2,7 +2,7 @@ project('libinput', 'c',
 	version : '1.29.0',
 	license : 'MIT/Expat',
 	default_options : [ 'c_std=gnu99', 'warning_level=2' ],
-	meson_version : '>= 0.56.0')
+	meson_version : '>= 0.64.0')
 
 libinput_version = meson.project_version().split('.')
 
@@ -168,7 +168,21 @@ dep_libevdev = dependency('libevdev', version: '>= 1.10.0')
 dep_lm = cc.find_library('m', required : false)
 dep_rt = cc.find_library('rt', required : false)
 
-config_h.set10('HAVE_PLUGINS', true)
+dep_lua = dependency('lua-5.4', 'lua5.4', 'lua',
+		     version : '>= 5.4',
+		     required : get_option('lua-plugins'))
+have_lua = dep_lua.found()
+config_h.set10('HAVE_LUA', have_lua)
+
+have_plugins =  dep_lua.found()
+config_h.set10('HAVE_PLUGINS', have_plugins)
+
+summary({
+	'Plugins enabled' : have_plugins,
+	'Lua Plugin support' : have_lua,
+	},
+	section : 'Plugins',
+	bool_yn : true)
 
 # Include directories
 includes_include = include_directories('include')
@@ -417,8 +431,15 @@ src_libinput = src_libfilter + [
 	'src/timer.c',
 	'src/util-libinput.c',
 ]
+
 if have_mtdev
 	src_libinput += ['src/libinput-plugin-mtdev.c']
+endif
+
+if dep_lua.found()
+	src_libinput += [
+		'src/libinput-plugin-lua.c',
+	]
 endif
 
 deps_libinput = [
@@ -430,7 +451,8 @@ deps_libinput = [
 	dep_rt,
 	dep_libwacom,
 	dep_libinput_util,
-	dep_libquirks
+	dep_libquirks,
+	dep_lua,
 ]
 
 libinput_version_h_config = configuration_data()
@@ -478,6 +500,8 @@ git_version_h = vcs_tag(command : ['git', 'describe'],
 			fallback : 'unknown',
 			input : 'src/libinput-git-version.h.in',
 			output :'libinput-git-version.h')
+
+subdir('plugins')
 
 ############ documentation ############
 
@@ -1024,6 +1048,10 @@ if get_option('tests')
 		'test/test-switch.c',
 		'test/test-quirks.c',
 	]
+	if have_plugins and have_lua
+		tests_sources += ['test/test-plugins-lua.c']
+	endif
+
 	libinput_test_runner_sources = litest_sources + tests_sources
 	libinput_test_runner = executable('libinput-test-suite',
 					  libinput_test_runner_sources,
@@ -1065,6 +1093,9 @@ if get_option('tests')
 		'trackpoint',
 		'udev',
 	]
+	if have_plugins and have_lua
+		collections += ['lua']
+	endif
 
 	foreach group : collections
 		test('libinput-test-suite-@0@'.format(group),

src/libinput-plugin.c
@@ -31,6 +31,7 @@
 #include "evdev-frame.h"
 #include "evdev-plugin.h"
 #include "libinput-plugin-button-debounce.h"
+#include "libinput-plugin-lua.h"
 #include "libinput-plugin-mouse-wheel-lowres.h"
 #include "libinput-plugin-mouse-wheel.h"
 #include "libinput-plugin-mtdev.h"
@@ -362,6 +363,18 @@ libinput_plugin_system_load_plugins(struct libinput *libinput,
 		log_bug_client(libinput, "%s() called twice\n", __func__);
 		return 0;
 	}
+
+#if HAVE_LUA
+	_autostrvfree_ char **directories = steal(&libinput->plugin_system.directories);
+	size_t nfiles = 0;
+	_autostrvfree_ char **plugin_files =
+		list_files((const char **)directories, ".lua", &nfiles);
+	for (size_t i = 0; i < nfiles; i++) {
+		char *path = plugin_files[i];
+		log_debug(libinput, "Loading plugin from %s\n", path);
+		libinput_lua_plugin_new_from_path(libinput, path);
+	}
+#endif
 
 	libinput_plugin_system_load_internal_plugins(libinput,
 						     &libinput->plugin_system);

doc/user/meson.build
@@ -155,6 +155,7 @@ src_rst = files(
 	'middle-button-emulation.rst',
 	'normalization-of-relative-motion.rst',
 	'palm-detection.rst',
+	'lua-plugins.rst',
 	'pointer-acceleration.rst',
 	'reporting-bugs.rst',
 	'scrolling.rst',

plugins/meson.build
@@ -1,0 +1,13 @@
+plugins = [
+	'10-example.lua',
+	'10-dwt.lua',
+	'10-logitech-mx-master-horiz-scroll.lua',
+	'10-pointer-go-faster.lua',
+	'10-pointer-go-slower.lua',
+	'10-delay-motion.lua',
+]
+
+fs = import('fs')
+foreach plugin : plugins
+	fs.copyfile(plugin)
+endforeach

.gitlab-ci.yml
@@ -98,11 +98,11 @@ variables:
   # See the documentation here:                                                 #
   # https://wayland.freedesktop.org/libinput/doc/latest/building.html           #
   ###############################################################################
-  FEDORA_PACKAGES:  'git-core gcc gcc-c++ pkgconf-pkg-config meson check-devel libudev-devel libevdev-devel doxygen graphviz python3-sphinx python3-recommonmark python3-sphinx_rtd_theme python3-pytest-xdist libwacom-devel cairo-devel gtk4-devel glib2-devel mtdev-devel diffutils wayland-protocols-devel black clang clang-tools-extra jq rpmdevtools valgrind systemd-udev qemu-img qemu-system-x86-core qemu-system-aarch64-core jq python3-click python3-rich virtme-ng'
-  DEBIAN_PACKAGES:  'git gcc g++ pkg-config meson check libudev-dev libevdev-dev doxygen graphviz python3-sphinx python3-recommonmark python3-sphinx-rtd-theme python3-pytest-xdist libwacom-dev libcairo2-dev libgtk-3-dev libglib2.0-dev libmtdev-dev curl'
-  UBUNTU_PACKAGES:  'git gcc g++ pkg-config meson check libudev-dev libevdev-dev doxygen graphviz python3-sphinx python3-recommonmark python3-sphinx-rtd-theme python3-pytest-xdist libwacom-dev libcairo2-dev libgtk-3-dev libglib2.0-dev libmtdev-dev'
-  ARCH_PACKAGES:    'git gcc pkgconfig meson check libsystemd libevdev python-pytest-xdist libwacom gtk4 mtdev diffutils'
-  ALPINE_PACKAGES:  'git gcc build-base pkgconfig meson check-dev eudev-dev libevdev-dev libwacom-dev cairo-dev gtk4.0-dev mtdev-dev bash'
+  FEDORA_PACKAGES:  'git-core gcc gcc-c++ pkgconf-pkg-config meson check-devel libudev-devel libevdev-devel doxygen graphviz python3-sphinx python3-recommonmark python3-sphinx_rtd_theme python3-pytest-xdist libwacom-devel cairo-devel gtk4-devel glib2-devel mtdev-devel diffutils wayland-protocols-devel black clang clang-tools-extra jq rpmdevtools valgrind systemd-udev qemu-img qemu-system-x86-core qemu-system-aarch64-core jq python3-click python3-rich virtme-ng lua-devel'
+  DEBIAN_PACKAGES:  'git gcc g++ pkg-config meson check libudev-dev libevdev-dev doxygen graphviz python3-sphinx python3-recommonmark python3-sphinx-rtd-theme python3-pytest-xdist libwacom-dev libcairo2-dev libgtk-3-dev libglib2.0-dev libmtdev-dev curl lua5.4-dev'
+  UBUNTU_PACKAGES:  'git gcc g++ pkg-config meson check libudev-dev libevdev-dev doxygen graphviz python3-sphinx python3-recommonmark python3-sphinx-rtd-theme python3-pytest-xdist libwacom-dev libcairo2-dev libgtk-3-dev libglib2.0-dev libmtdev-dev lua5.4-dev'
+  ARCH_PACKAGES:    'git gcc pkgconfig meson check libsystemd libevdev python-pytest-xdist libwacom gtk4 mtdev diffutils lua'
+  ALPINE_PACKAGES:  'git gcc build-base pkgconfig meson check-dev eudev-dev libevdev-dev libwacom-dev cairo-dev gtk4.0-dev mtdev-dev bash lua5.4-dev'
   FREEBSD_PACKAGES: 'git pkgconf meson libepoll-shim libudev-devd libevdev libwacom gtk3 libmtdev bash wayland'
   ############################ end of package lists #############################
 
@@ -110,12 +110,12 @@ variables:
   # changing these will force rebuilding the associated image
   # Note: these tags have no meaning and are not tied to a particular
   # libinput version
-  FEDORA_TAG:  '2025-05-19.0'
-  DEBIAN_TAG:  '2025-05-19.0'
-  UBUNTU_TAG:  '2025-05-19.0'
-  ARCH_TAG:    '2025-05-19.0'
-  ALPINE_TAG:  '2025-05-19.0'
-  FREEBSD_TAG: '2025-05-19.0'
+  FEDORA_TAG:  '2025-05-20.0'
+  DEBIAN_TAG:  '2025-05-20.0'
+  UBUNTU_TAG:  '2025-05-20.0'
+  ARCH_TAG:    '2025-05-20.0'
+  ALPINE_TAG:  '2025-05-20.0'
+  FREEBSD_TAG: '2025-05-20.0'
 
   FDO_UPSTREAM_REPO: libinput/libinput
 
@@ -796,6 +796,19 @@ vm-pointer-no-libwacom:
   variables:
     MESON_ARGS: '-Dlibwacom=false'
 
+vm-lua:
+  extends:
+    - .fedora:42@test-suite-vm
+  variables:
+    SUITE_NAMES: 'lua'
+
+vm-lua-no-libwacom:
+  extends:
+    - vm-lua
+  stage: test-suite-no-libwacom
+  variables:
+    MESON_ARGS: '-Dlibwacom=false'
+
 
 vm-valgrind-touchpad:
   stage: valgrind
@@ -1005,6 +1018,20 @@ vm-valgrind-pointer:
   rules:
     - if: $GITLAB_USER_LOGIN != "marge-bot"
 
+vm-valgrind-lua:
+  stage: valgrind
+  extends:
+    - vm-lua
+    - .policy-retry-on-failure
+  variables:
+    MESON_TEST_ARGS: '--setup=valgrind'
+    LITEST_JOBS: 0
+  retry:
+    max: 2
+  rules:
+    - if: $GITLAB_USER_LOGIN != "marge-bot"
+
+
 
 .fedora-build@template:
   extends:
@@ -1066,6 +1093,20 @@ build-no-mtdev-nodeps@fedora:42:
     MESON_ARGS: "-Dmtdev=false"
   before_script:
     - dnf remove -y mtdev mtdev-devel
+
+build-no-lua@fedora:42:
+  extends:
+    - .fedora-build@template
+  variables:
+    MESON_ARGS: "-Dlua-plugins=disabled"
+
+build-no-lua-nodeps@fedora:42:
+  extends:
+    - .fedora-build@template
+  variables:
+    MESON_ARGS: "-Dlua-plugins=disabled"
+  before_script:
+    - dnf remove -y lua lua-devel
 
 build-docs@fedora:42:
   extends:
@@ -1163,6 +1204,7 @@ check-test-suites:
         libinput-test-suite-totem
         libinput-test-suite-touch
         libinput-test-suite-pointer
+        libinput-test-suite-lua
       EOF
     - sort -o ci-testsuites ci-testsuites
     - diff -u8 -w ci-testsuites meson-testsuites || (echo "Some test suites are not run in the CI" && false)

.gitlab-ci/ci.template
@@ -492,6 +492,7 @@ vm-valgrind-{{suite.name}}:
     - if: $GITLAB_USER_LOGIN != "marge-bot"
 
 {% endfor %}
+
 {% endfor %}{# for if distro.use_for_qemu_tests #}
 
 {% for distro in distributions if distro.use_for_custom_build_tests %}
@@ -556,6 +557,20 @@ build-no-mtdev-nodeps@{{distro.name}}:{{version}}:
     MESON_ARGS: "-Dmtdev=false"
   before_script:
     - dnf remove -y mtdev mtdev-devel
+
+build-no-lua@{{distro.name}}:{{version}}:
+  extends:
+    - .{{distro.name}}-build@template
+  variables:
+    MESON_ARGS: "-Dlua-plugins=disabled"
+
+build-no-lua-nodeps@{{distro.name}}:{{version}}:
+  extends:
+    - .{{distro.name}}-build@template
+  variables:
+    MESON_ARGS: "-Dlua-plugins=disabled"
+  before_script:
+    - dnf remove -y lua lua-devel
 
 build-docs@{{distro.name}}:{{version}}:
   extends:

.gitlab-ci/config.yml
@@ -3,7 +3,7 @@
 #
 
 # We're happy to rebuild all containers when one changes.
-.default_tag: &default_tag '2025-05-19.0'
+.default_tag: &default_tag '2025-05-20.0'
 
 distributions:
   - name: fedora
@@ -50,6 +50,7 @@ distributions:
       - python3-click
       - python3-rich
       - virtme-ng
+      - lua-devel
   - name: debian
     tag: *default_tag
     versions:
@@ -75,6 +76,7 @@ distributions:
       - libglib2.0-dev
       - libmtdev-dev
       - curl            # for the coverity job
+      - lua5.4-dev
   - name: ubuntu
     tag: *default_tag
     versions:
@@ -99,6 +101,7 @@ distributions:
       - libgtk-3-dev
       - libglib2.0-dev
       - libmtdev-dev
+      - lua5.4-dev
   - name: arch
     tag: *default_tag
     versions:
@@ -116,6 +119,7 @@ distributions:
       - gtk4
       - mtdev
       - diffutils
+      - lua
     build:
       extra_variables:
         - "MESON_ARGS: '-Ddocumentation=false'" # python-recommonmark is no longer in the repos
@@ -136,6 +140,7 @@ distributions:
       - gtk4.0-dev
       - mtdev-dev
       - bash
+      - lua5.4-dev
     build:
       extra_variables:
         - "MESON_ARGS: '-Ddocumentation=false' # alpine does not have python-recommonmark"
@@ -227,6 +232,9 @@ test_suites:
   - name: pointer
     suites:
       - pointer
+  - name: lua
+    suites:
+      - lua
 
 vng:
   kernel: https://gitlab.freedesktop.org/api/v4/projects/libevdev%2Fhid-tools/packages/generic/kernel-x86_64/v6.14/bzImage

plugins/10-delay-motion.lua
@@ -1,0 +1,61 @@
+-- SPDX-License-Identifier: MIT
+--
+-- This is an example libinput plugin
+--
+-- This plugin delays any event with relative motion by the given DELAY
+-- by storing it in a table and replaying it via a timer callback later.
+
+-- UNCOMMENT THIS LINE TO ACTIVATE THE PLUGIN
+-- libinput:register({1})
+
+DELAY = 1500 * 1000 -- 1.5s
+next_timer_expiry = 0
+devices = {}
+
+function timer_expired(time_in_microseconds)
+    next_timer_expiry = 0
+    for device, frames in pairs(devices) do
+        while #frames > 0 and frames[1].time <= time_in_microseconds do
+            --- we don't have a current frame so it doesn't matter
+            --- whether we prepend or append
+            device:prepend_frame(frames[1].frame)
+            table.remove(frames, 1)
+        end
+        local next_frame = frames[1]
+        if next_frame and (next_timer_expiry == 0 or next_frame.time < next_timer_expiry) then
+            next_timer_expiry = next_frame.time
+        end
+    end
+    if next_timer_expiry ~= 0 then
+        libinput:timer_set_absolute(next_timer_expiry)
+    end
+end
+
+function frame(device, frame, timestamp)
+    for _, v in ipairs(frame) do
+        if v.usage == evdev.REL_X or v.usage == evdev.REL_Y then
+            next_time = timestamp + DELAY
+            table.insert(devices[device], {
+                time = next_time,
+                frame = frame
+            })
+            if next_timer_expiry == 0 then
+                next_timer_expiry = next_time
+                libinput:timer_set_absolute(next_timer_expiry)
+            end
+            return {} -- discard frame
+        end
+    end
+    return nil
+end
+
+function device_new(device)
+    local usages = device:usages()
+    if usages[evdev.REL_X] then
+        devices[device] = {}
+        device:connect("evdev-frame", frame)
+    end
+end
+
+libinput:connect("new-evdev-device", device_new)
+libinput:connect("timer-expired", timer_expired)

commit f27fbdfa53d6de802bbb4cc9cfbfdc3ddc7a4ded

meson.build: change set10 to set
Follow-up to cfec80582efc ("meson.build: change from config.set10() and #if to config.set() and #ifdef")
which was parallel to 9e37bc0cfa4d and the latter didn't get updated.

Fixes: 9e37bc0cfa4d ("plugins: add support for lua plugins to change evdev event streams")
Part-of: <https://gitlab.freedesktop.org/libinput/libinput/-/merge_requests/1347>

meson.build
@@ -171,10 +171,14 @@ dep_rt = cc.find_library('rt', required : false)
 dep_lua = dependency(get_option('lua-interpreter'),
 		     required : get_option('lua-plugins'))
 have_lua = dep_lua.found()
-config_h.set10('HAVE_LUA', have_lua)
+if have_lua
+	config_h.set('HAVE_LUA', 1)
+endif
 
 have_plugins =  dep_lua.found()
-config_h.set10('HAVE_PLUGINS', have_plugins)
+if have_plugins
+	config_h.set('HAVE_PLUGINS', 1)
+endif
 
 summary({
 	'Plugins enabled' : have_plugins,

src/libinput-plugin.c
@@ -382,7 +382,7 @@ libinput_plugin_system_load_plugins(struct libinput *libinput,
 		return 0;
 	}
 
-#if HAVE_LUA
+#ifdef HAVE_LUA
 	_autostrvfree_ char **directories = steal(&libinput->plugin_system.directories);
 	size_t nfiles = 0;
 	_autostrvfree_ char **plugin_files =
@@ -400,10 +400,10 @@ libinput_plugin_system_load_plugins(struct libinput *libinput,
 
 	libinput_plugin_system_run(&libinput->plugin_system);
 
-#if !HAVE_PLUGINS
-	return -ENOSYS;
-#else
+#ifdef HAVE_PLUGINS
 	return 0;
+#else
+	return -ENOSYS;
 #endif
 }
 