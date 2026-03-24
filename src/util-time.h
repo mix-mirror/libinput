mix-mirror/libinput

commit dcbfbc4cf1c0d6f6801760e09c49448c1e2a60e3

util: fix usec computation on 32 bits
Enforce the needed 64-bit computing on 32 bits.

Signed-off-by: GKraats <vd.kraats@hccnet.nl>
Part-of: <https://gitlab.freedesktop.org/libinput/libinput/-/merge_requests/1446>

src/util-time.h
@@ -41,7 +41,7 @@ DECLARE_NEWTYPE(usec, uint64_t);
 static inline usec_t
 usec_from_millis(uint32_t millis)
 {
-	return usec_from_uint64_t(millis * 1000);
+	return usec_from_uint64_t(millis * 1000ULL);
 }
 
 static inline usec_t
@@ -101,13 +101,13 @@ us2ms_f(usec_t us)
 static inline usec_t
 usec_from_timeval(const struct timeval *tv)
 {
-	return usec_from_uint64_t(tv->tv_sec * 1000000 + tv->tv_usec);
+	return usec_from_uint64_t(tv->tv_sec * 1000000ULL + tv->tv_usec);
 }
 
 static inline usec_t
 usec_from_timespec(const struct timespec *tp)
 {
-	return usec_from_uint64_t(tp->tv_sec * 1000000 + tp->tv_nsec / 1000);
+	return usec_from_uint64_t(tp->tv_sec * 1000000ULL + tp->tv_nsec / 1000);
 }
 
 static inline usec_t

commit 166201d9fca04736c7843fa61a597ac6d12e4e7d

util: fix remaininig usec computation on 32 bits
Follow-up from commit dcbfbc4cf1c0 ("util: fix usec computation on 32 bits")
which didn't fix all instances, make this consistent.

Fixes: dcbfbc4cf1c0 ("util: fix usec computation on 32 bits")
Part-of: <https://gitlab.freedesktop.org/libinput/libinput/-/merge_requests/1453>

src/util-time.h
@@ -47,13 +47,13 @@ usec_from_millis(uint32_t millis)
 static inline usec_t
 usec_from_seconds(uint32_t secs)
 {
-	return usec_from_millis(secs * 1000);
+	return usec_from_uint64_t(secs * 1000000ULL);
 }
 
 static inline usec_t
 usec_from_hours(uint32_t hours)
 {
-	return usec_from_seconds(hours * 3600);
+	return usec_from_uint64_t((uint64_t)hours * 3600 * 1000000ULL);
 }
 
 static inline uint32_t
@@ -83,7 +83,7 @@ usec_to_hours(usec_t us)
 static inline usec_t
 usec_add_millis(usec_t us, uint32_t millis)
 {
-	return usec_from_uint64_t(usec_as_uint64_t(us) + millis * 1000);
+	return usec_from_uint64_t(usec_as_uint64_t(us) + millis * 1000ULL);
 }
 
 static inline usec_t