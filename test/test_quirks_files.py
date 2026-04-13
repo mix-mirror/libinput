mix-mirror/libinput

commit 488c0c96455a7baa773b2c94068f1eb7d6e528b7

quirks: add quirk for the Wareus B15
Fix:
https://gitlab.freedesktop.org/libinput/libinput/-/work_items/1267

Part-of: <https://gitlab.freedesktop.org/libinput/libinput/-/merge_requests/1457>

quirks/50-system-wareus.quirks
@@ -1,0 +1,6 @@
+# Clickpad that announces BTN_RIGHT
+[Wareus B15 Touchpad]
+MatchName=HTIX5288:00 36B6:C001 Touchpad
+MatchUdevType=touchpad
+MatchDMIModalias=dmi:*svnWareus:*pnB15*
+AttrEventCode=-BTN_RIGHT

commit 8c06ceecb283159d3a9dc275fc5af1085a1974d8

quirks: add missing modification warning to a quirks file
And add a test to make sure we don't forget this in the future.

Fixes: 488c0c96455a ("quirks: add quirk for the Wareus B15")
Part-of: <https://gitlab.freedesktop.org/libinput/libinput/-/merge_requests/1464>

quirks/50-system-wareus.quirks
@@ -1,3 +1,5 @@
+# Do not edit this file, it will be overwritten on update
+
 # Clickpad that announces BTN_RIGHT
 [Wareus B15 Touchpad]
 MatchName=HTIX5288:00 36B6:C001 Touchpad

test/test_quirks_files.py
@@ -44,6 +44,14 @@ def pytest_generate_tests(metafunc):
         metafunc.parametrize("quirksfile", [f for f in quirksdir().glob("*.quirks")])
 
 
+def test_has_modification_warning(quirksfile):
+    with open(quirksfile) as f:
+        first_line = f.readline().strip()
+    assert first_line == "# Do not edit this file, it will be overwritten on update", (
+        f"missing header in file {quirksfile}"
+    )
+
+
 def test_matches_are_valid(quirksfile):
     quirks = configparser.ConfigParser(strict=True)
     # Don't convert to lowercase